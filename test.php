a24a01fe7288d24c02b3e9ac476e84fa1fc38a05
Nurasyl <nurasylmelsuly@gmail.com>
2024-07-26 21:09:48 +0500

    Update script

--- test.php
+++ test.php
@@ -1,15 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-100" lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Тест</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .box {
+            max-width: 768px;
+            width: 100%;
+            padding: 32px;
+        }
+    </style>
 </head>
-<body>
-    <form action="/">
-        <input type="text" name="name" />
+<body class="bg-dark text-white h-100">
+    <div class="d-flex h-100 justify-content-center align-items-center">
+        <div class="box border rounded border-dark-subtle">
+            <form action="/" method="post">
 
-        <button type="submit">Submit</button>
-    </form>
+            <h2>
+                Форма для теста
+            </h2>
+
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email адрес</label>
+                <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+                Отправить
+            </button>
+
+            </form>
+
+
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
